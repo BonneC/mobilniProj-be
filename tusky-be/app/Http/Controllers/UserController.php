a63af588e0c3034c2af8950577ac8bba02ded52a
BonneC <bonnegr33n@gmail.com>
2019-02-28 04:51:47 +0100

    lil bit of crud

---
+++ tusky-be/app/Http/Controllers/UserController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        return $request->user();
+    }
+
+    /**
+     * Store a newly created user in storage
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+        $newUser = User::create([
+            'username' => $request->json('username'),
+            'email' => $request->json('email'),
+            'password' => bcrypt($request->json('password'))
+        ]);
+
+        // get token to provide it for auto-login after sign-up
+        $credentials = request(['email', 'password']);
+        $token = auth()->attempt($credentials);
+        // if creation is successful
+        if ($newUser) {
+            return response()->json([
+                'success' => true,
+                'access_token' => $token,
+                'expires_in' => auth()->factory()->getTTL() * 60
+            ], 201);
+        }
+
+
+        return response()->json(['success' => false], 500);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //TODO
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
--- tusky-be/app/Topic.php
+++ tusky-be/app/Topic.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Topic extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'description',
+    ];
+
+    /**
      * Return all users that subscribe to this topic
      */
     public function users()
